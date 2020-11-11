a94962955cff875c63b0998719590bd5898bd971
Armen <upravlenciarmen@mail.ru>
2020-11-11 17:19:46 +0400

    contacts complited :) page is empty

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -2,8 +2,14 @@ import React from "react"
 
 
 
+
 export const Contact = () => {
     return (
-        <>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</>
+        <div>
+            <div>location</div>
+            <div>phone</div>
+            <div>mail</div>
+            <div>{`not an interesting page too lazy to create :)`}</div>
+        </div>
     )
 }
