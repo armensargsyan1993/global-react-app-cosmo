c42e82426cae4f9cc0fdbda6c628157116b02bd7
Armen <upravlenciarmen@mail.ru>
2020-11-13 02:14:55 +0400

    complited

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import './index.scss';
 import App from './App';
-import { HashRouter ,BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 
 const store = createStore(rootReducer, composeWithDevTools(
